4e89756875eaf6c9013737542c9e7a8b6488eb87
Michał Jaworski <jawor403@gmail.com>
2020-05-15 00:25:22 +0200

    Proper data set loading, including normalization

--- utils/data.py
+++ utils/data.py
@@ -3,28 +3,41 @@ from base.data_preprocessing import BaseDataPreprocessing
 
 
 def get_train_valid_data(config, preprocessing: BaseDataPreprocessing):
-    d = _get_data(config, ["train", "validation"])
-    d = {
-        "train": d[0].map(preprocessing.preprocess_train),
-        "valid": d[1].map(preprocessing.preprocess_test),
-    }
-    if config.data.shuffle:
-        d["train"] = d["train"].shuffle(config.data.get("shuffle_buffer_size", 6000))
-    return d
+
+    def preprocess_train(datapoint):
+        return preprocessing.preprocess_train(datapoint)
+
+    def preprocess_test(datapoint):
+        return preprocessing.preprocess_test(datapoint)
+
+    d = _get_data(config, ["train[:90%]", "train[90%:]"],
+                  shuffle_files=config.data.get("shuffle", False))
+    d = [d[0].map(preprocess_train), d[1].map(preprocess_test)]
+    d = [s.prefetch(config.data.get("prefetch_buffer", 10)) for s in d]
+    return {"train": d[0], "valid": d[1]}
 
 
 def get_test_data(config, preprocessing: BaseDataPreprocessing):
-    return _get_data(config, "test").map(preprocessing.preprocess_test)
+
+    def preprocess_test(datapoint):
+        return preprocessing.preprocess_test(datapoint)
+
+    return _get_data(config, "validation") \
+        .map(preprocess_test) \
+        .prefetch(config.data.get("prefetch_buffer", 10))
 
 
-def _get_data(config, splits):
+def _get_data(config, splits, shuffle_files=False):
     data_dir = config.data.get("data_dir", "data")
 
     splits = tfds.load(
-        "caltech_birds2011",
+        "cityscapes",
         data_dir=data_dir,
         batch_size=config.data.batch_size,
         split=splits,
-        download=False
+        download=False,
+        as_dataset_kwargs=dict(
+            shuffle_files=shuffle_files
+        ),
     )
     return splits
