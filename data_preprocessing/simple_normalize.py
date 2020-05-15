ce15d2abec014a56d02eef2f6e36b6afcc17ee56
Michał Jaworski <jawor403@gmail.com>
2020-05-15 20:08:12 +0200

    Store input shape for the model

--- data_preprocessing/simple_normalize.py
+++ data_preprocessing/simple_normalize.py
@@ -2,17 +2,21 @@ from base.data_preprocessing import BaseDataPreprocessing
 import tensorflow as tf
 
 
+def _normalize(input_image):
+    input_image = tf.cast(input_image, tf.float64) / 127.5 - 1.
+    return input_image
+
+
 class SimpleNormalize(BaseDataPreprocessing):
-    @staticmethod
-    def normalize(input_image):
-        input_image = tf.cast(input_image, tf.float64) / 127.5 - 1.
-        return input_image
+    def __init__(self, config):
+        super().__init__(config)
+        self.config.model.input_shape = (*self.config.data_preprocessing.image_size, 3)
 
     def preprocess(self, datapoint):
         image_size = tuple(self.config.data_preprocessing.image_size)
         input_image = tf.image.resize(datapoint['image_left'], image_size)
         input_mask = tf.image.resize(datapoint['segmentation_label'], image_size, method="nearest")
 
-        input_image = self.normalize(input_image)
+        input_image = _normalize(input_image)
 
         return input_image, input_mask
